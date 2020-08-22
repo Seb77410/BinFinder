6c8297736f10002d7f94061e483756a71dd6d02a
seb77410 <seb_perso@hotmail.fr>
2020-08-22 13:31:45 +0200

    Modify travis.yml

--- app/src/androidTest/java/com/application/seb/binfinder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/application/seb/binfinder/ExampleInstrumentedTest.java
@@ -2,12 +2,20 @@ package com.application.seb.binfinder;
 
 import android.content.Context;
 
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +25,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public IntentsTestRule<SignInActivity> intentsTestRule = new IntentsTestRule<>(SignInActivity.class);
+
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.application.seb.binfinder", appContext.getPackageName());
+    public void sign_in_test(){
+        onView(withId(R.id.sign_in_activity_google_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.sign_in_activity_image)).check(matches(isDisplayed()));
+        onView(withId(R.id.sign_in_activity_mail_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.sign_in_activity_skip_button)).check(matches(isDisplayed()));
+
     }
 }
